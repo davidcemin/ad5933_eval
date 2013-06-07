17bd3a7b241bde4ef3f5c7d038b5c1092851e948
David Cemin <davidcemin@gmail.com>
2013-06-06 18:02:17 -0700

    calibration is now working

--- ad5933/ad5933.c
+++ ad5933/ad5933.c
@@ -49,7 +49,7 @@ ad5933_reg_write(usb_dev_handle *h, unsigned char addr, unsigned char val, unsig
 	unsigned char dum = val & 0xff;
 
 	printf("addr 0x%x data 0x%x\n\r", addr, (val<<8) | addr);
-	ret = usb_control_msg(h, 0x40, 0xde, 0x0d, val << 8 | reg, &dum, sz, 1000);
+	ret = usb_control_msg(h, 0x40, 0xde, 0x0d, val << 8 | addr, &dum, sz, 1000);
 
 	return (ret);
 }
@@ -65,6 +65,7 @@ ad5933_reg_read(usb_dev_handle *h, unsigned char reg, unsigned char *val, unsign
 
 	return (ret);
 }
+
 /********************************************************************************/
 
 static inline char
@@ -72,13 +73,33 @@ ad5933_get_status(usb_dev_handle *h)
 {
 	char sts_reg = 0x0;
 
-	usb_control_msg(h, 0xc0, 0xde, 0x0d, AD5933_STS_REG, &sts_reg, 1, 1000);
+	ad5933_reg_read(h, AD5933_STS_REG, &sts_reg, 1);
+
 	printf("status reg = 0x%x\n\r", sts_reg);
 
 	return (sts_reg);
 }
 
 /********************************************************************************/
+
+static inline char
+ad5933_reset(usb_dev_handle *h)
+{
+	unsigned char ctrl_lsb = 0x00;
+
+	ad5933_reg_read(h, AD5933_CTRL_REG_LSB, &ctrl_lsb, 1);
+
+	ctrl_lsb |= MASK_RESET_TRUE;
+	ad5933_reg_write(h, AD5933_CTRL_REG_LSB, ctrl_lsb, 1);
+	ad5933_reg_read(h, AD5933_CTRL_REG_LSB, &ctrl_lsb, 1);
+
+	//printf("CTRL LSB = 0x%x\n", ctrl_lsb);
+
+
+	return (0);
+}
+
+/****************************************************************************/
 
 static inline char
 ad5933_imped_read(usb_dev_handle *h, st_imped_data_raw_t *ir)
@@ -116,7 +137,6 @@ ad5933_start_freq_calc(usb_dev_handle *h, double freq)
 	unsigned char f1 = 0;
 	unsigned char f2 = 0;
 	unsigned char f3 = 0;
-	unsigned char f = 0;
 
 	freq_code = freq/(INT_CLK_FREQ*250)*pow(2,27);
 
@@ -126,15 +146,71 @@ ad5933_start_freq_calc(usb_dev_handle *h, double freq)
 	f2 = (freq_code >> 8) & 0xff;
 	f3 = (freq_code) & 0xff;
 
-	ad5933_reg_write(h, AD5933_START_FREQ1, f1, 0);
-	ad5933_reg_read(h, AD5933_START_FREQ1, &f, 1);
-	printf("FREQ READ: 0x%x\n", f);
+	ad5933_reg_write(h, AD5933_START_FREQ1, f1, 1);
 	ad5933_reg_write(h, AD5933_START_FREQ2, f2, 1);
 	ad5933_reg_write(h, AD5933_START_FREQ3, f3, 1);
+
+	return (0);
 }
 
 /********************************************************************************/
 
+static inline char
+ad5933_numb_incr_set(usb_dev_handle *h, unsigned int inc)
+{
+	unsigned char msb = (inc >> 8) & 0xff;
+	unsigned char lsb = inc & 0xff;
+
+	ad5933_reg_write(h, AD5933_NUMB_INCR_MSB, msb, 1);
+	ad5933_reg_write(h, AD5933_NUMB_INCR_LSB, lsb, 1);
+
+	return (0);
+}
+
+/****************************************************************************/
+
+/*step is in hertz*/
+static inline char
+ad5933_freq_inc_set(usb_dev_handle *h, double step)
+{
+	unsigned int step_code = 0;
+	unsigned char f1 = 0;
+	unsigned char f2 = 0;
+	unsigned char f3 = 0;
+
+	step_code = pow(2,27)*step*4/(INT_CLK_FREQ*pow(10,6));
+
+	f1 = (step_code >> 16) & 0xff;
+	f2 = (step_code >> 8) & 0xff;
+	f3 = (step_code) & 0xff;
+
+	printf("S = 0x%x\n\r", step_code);
+
+	ad5933_reg_write(h, AD5933_FREQ_INC1, f1, 1);
+	ad5933_reg_write(h, AD5933_FREQ_INC2, f2, 1);
+	ad5933_reg_write(h, AD5933_FREQ_INC3, f3, 1);
+
+	return (0);
+}
+
+/****************************************************************************/
+
+/*mult can be 0, 1 or 3*/
+static inline char
+ad5933_settling_set(usb_dev_handle *h, unsigned int s, unsigned char mult)
+{
+	unsigned char msb = (mult << 3) | ( (s << 8) & 0xff );
+	unsigned char lsb = s & 0xff;
+
+	ad5933_reg_write(h, AD5933_NUMB_SETT_MSB, msb, 1);
+	ad5933_reg_write(h, AD5933_NUMB_SETT_LSB, lsb, 1);
+
+	return (0);
+}
+
+/****************************************************************************/
+
+/*gf = gfe-9;*/
 static inline char
 ad5933_imped_calibration(usb_dev_handle *h, double *gf)
 {
@@ -146,22 +222,77 @@ ad5933_imped_calibration(usb_dev_handle *h, double *gf)
 	double m2 = 0.0;
 	short real = 0x0000;
 	short imag = 0x0000;
+	unsigned char ctrl_reg = 0;
+	unsigned int steps = 200;
+	unsigned int inc = 1;
+	unsigned int i = 0;
+	unsigned char sweep_ok = 0;
 
-	ad5933_start_freq_calc(h, 30);
+	/*AD5933.pdf page 22*/
+	/*1: Start frequency register*/
+	ad5933_start_freq_calc(h, 60);
+	/*2: Number of increments register*/
+	ad5933_numb_incr_set(h, steps);
+	/*3: Frequency increment register*/
+	ad5933_freq_inc_set(h, 1);
+	/* Settling time*/
+	ad5933_settling_set(h, 15, 0);
+
+	/*Place ad5933 in standby mode*/
+	ad5933_reset(h);
+	ad5933_reg_read(h, AD5933_CTRL_REG_MSB, &ctrl_reg, 1);
+	ctrl_reg |= MASK_SB_MODE;
+	ad5933_reg_write(h, AD5933_CTRL_REG_MSB, ctrl_reg, 1);
+
+	/*Start Frequency*/
+	ctrl_reg = MASK_INIT_START_FREQ | MASK_OUTPUT_2Vpp | MASK_PGA_GAIN5x;
+	ad5933_reg_write(h, AD5933_CTRL_REG_MSB, ctrl_reg, 1);
+	usleep(2000);
+
+	/*Calculate first point*/
 	ad5933_imped_read(h, &ir);
 	m1 = ad5933_magnitude_calc(ir.real_msb, ir.real_lsb, ir.imag_msb, ir.imag_lsb);
 
-	ad5933_start_freq_calc(h, 60.2);
-	ad5933_imped_read(h, &ir);
-	m2 = ad5933_magnitude_calc(ir.real_msb, ir.real_lsb, ir.imag_msb, ir.imag_lsb);
+	/*start sweep*/
+	ctrl_reg = MASK_START_FREQ_SWEEP | MASK_OUTPUT_2Vpp | MASK_PGA_GAIN5x;
+	ad5933_reg_write(h, AD5933_CTRL_REG_MSB, ctrl_reg, 1);
 
+	ctrl_reg = 0;
+	ad5933_reg_read(h, AD5933_CTRL_REG_MSB, &ctrl_reg, 1);
+
+	while ( sweep_ok != MASK_STS_FRQ_SWP_RDY)
+	{
+		unsigned char sts = 0;
+		unsigned char ctrl_inc = 0;
+		int j = 0;
+
+		ad5933_reg_read(h, AD5933_CTRL_REG_MSB, &ctrl_inc, 1);
+		sts = ad5933_get_status(h);
+		while ( ( sts & MASK_STS_IMPED_VALID ) != MASK_STS_IMPED_VALID) {
+			printf("waiting imp valid..0x%x %d\n\r", sts, j);
+			usleep(1000);
+			j++;
+			if (j==11)
+				break;
+		}
+		ad5933_imped_read(h, &ir);
+		m2 = ad5933_magnitude_calc(ir.real_msb, ir.real_lsb, ir.imag_msb, ir.imag_lsb);
+		printf("m1 = %04f m2 = %04f\n\r", m1, m2);
+
+		i += inc;
+		ctrl_inc |= MASK_INC_FREQ;
+		ad5933_reg_write(h, AD5933_CTRL_REG_MSB, ctrl_inc, 1);
+		sweep_ok = ad5933_get_status(h) & MASK_STS_FRQ_SWP_RDY;
+		printf("sweep ok 0x%x \n\r", sweep_ok);
+	}
+
+	printf("-------------------------------------\n\r");
 	printf("%04f %04f\n\r", m1, m2);
+	printf("-------------------------------------\n\r");
 
 	/*two measures, we are considering that it varies linearly with the frequency.
 	 * The smaller the difference (delta F) in frequency, the better */
 	*gf = (pow(10,9)/AD5933_RES_FEEDBACK_VAL)/( ((m2-m1)/2) + m2);
-
-	printf("%04f\n", *gf);
 
 	return (ret);
 }
@@ -206,9 +337,9 @@ ad5933_get_temperature(usb_dev_handle *h)
 	 * if negative (bit 13 = 1), temp = (adc - 16384)/32
 	 */
 	if (t0 >> 5)
-		temp = ( ( ((t0&0xff)<<8) | t1&0xff ) - 16384) >> 5;
+		temp = ( ( ((t0&0xff)<<8) | (t1&0xff) ) - 16384) >> 5;
 	else
-		temp = ( ((t0&0xff)<<8) |  t1&0xff ) >> 5;
+		temp = ( ((t0&0xff)<<8) |  (t1&0xff) ) >> 5;
 
 	printf("0x%x 0x%x %d \n", t0, t1, temp);
 
@@ -273,16 +404,18 @@ main (int argc, char **argv)
 		return(1);
 	
 	usb_set_configuration(usbdevhandle,1);
-    usb_claim_interface(usbdevhandle,0);
-    usb_set_altinterface(usbdevhandle,1);
+	usb_claim_interface(usbdevhandle,0);
+	usb_set_altinterface(usbdevhandle,1);
 
 	//fprintf(stdout, "Programming 8051 using %s \n", file);
 	//err += xusb_program_hex_file(file, usbdevhandle);
 
-	ad5933_imped_calibration(usbdevhandle, &gf);
+	ad5933_reset(usbdevhandle);
 	ad5933_get_temperature(usbdevhandle);
+	ad5933_imped_calibration(usbdevhandle, &gf);
 	ad5933_imped_calc(usbdevhandle, &imped, gf);
 
+	printf("GF = %04f\n\r", gf);
 	printf("IMP = %04f\n\r", imped.magnitude);
 
     //usb_close(usbdevhandle);
